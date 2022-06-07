53b5420161829a5d16c30a0e3de971a5240b6477
Simon0231 <106628453+Simon0231@users.noreply.github.com>
2022-06-07 14:21:37 +0200

    Update airquality.py

--- RASPBERRY 1/CODES/airquality.py
+++ RASPBERRY 1/CODES/airquality.py
@@ -1,44 +1,11 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# The MIT License (MIT)
-#
-# Grove Base Hat for the Raspberry Pi, used to connect grove sensors.
-# Copyright (C) 2018  Seeed Technology Co.,Ltd.
-'''
-This is the code for
-    - `Grove -  Air Quality Sensor <https://www.seeedstudio.com/Grove-Air-quality-sensor-v1.3-p-2439.html)>`_
-
-Examples:
-
-    .. code-block:: python
-
-        import time
-        from grove.grove_air_quality_sensor_v1_3 import GroveAirQualitySensor
-
-        # connect to alalog pin 2(slot A2)
-        PIN = 2
-
-        sensor = GroveAirQualitySensor(pin)
-
-        print('Detecting ...') 
-        while True:
-            value = sensor.value        
-            if value > 100:
-                print("{}, High Pollution.".format(value))
-            else:
-                print("{}, Air Quality OK.".format(value))
-            time.sleep(.1)
-'''
 import time, sys
 from grove.adc import ADC 
 
 __all__ = ["GroveAirQualitySensor"]
 
 class GroveAirQualitySensor(object):
-
-
-    def __init__(self, channel): #numéro de la branche de la broche auquel le capteur est connecté
+    
+def __init__(self, channel): #numéro de la branche de la broche auquel le capteur est connecté
         self.channel = channel
         self.adc = ADC()
 
@@ -47,9 +14,9 @@ class GroveAirQualitySensor(object):
         return self.adc.read(self.channel) #renvoie la valeur de la qualité de l'air
 
 def main():
-    from grove.helper import SlotHelper
-    sh = SlotHelper(SlotHelper.ADC)
-    pin = sh.argv2pin()
+    from grove.helper import SlotHelper 
+    sh = SlotHelper(SlotHelper.ADC) #La fonction sert à connecter le capteur airqualitysensor à la raspberry en ADC 
+    pin = sh.argv2pin() #La fonction permet de se connecter à la pin  
 
     sensor = GroveAirQualitySensor(pin)
 
@@ -57,10 +24,10 @@ def main():
     if value > 150: #si la valeur est supréireur à 150 affiché l'air est polluée
         print(value, "l'air est pollué")
     else: # sinon afficher l'air est sain
-        print(value, "l'air est sain")
 
-
-if __name__ == '__main__':
+if __name__ == '__main__': #Fonction permettant d'éxecuter le main
     main()
+   
 
+ 
  
