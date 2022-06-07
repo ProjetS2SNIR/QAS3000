2b3842fe1db0a0a22df44b5d9733ce90cd8b18d2
Simon0231 <106628453+Simon0231@users.noreply.github.com>
2022-06-07 13:55:10 +0200

    Update dht.py

--- RASPBERRY 1/CODES/dht.py
+++ RASPBERRY 1/CODES/dht.py
@@ -1,31 +1,4 @@
-#!/usr/bin/python
-
-# Copyright (c) 2014 Adafruit Industries
-# Author: Tony DiCola
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-
-
-
-
-import Adafruit_DHT #insertion de la librairie Adafruit_DHT
+import Adafruit_DHT #Insertion de la librairie Adafruit_DHT
 
 sensor = Adafruit_DHT.DHT22 # Le capteur est réglé sur sur Adafruit_DHT.DHT22
 
@@ -33,9 +6,9 @@ pin = 4 # Connecté à la broche Gpio 4
 
 humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
-# Affichage de valeur de température et d'humidité	
+   
 if humidity is not None and temperature is not None:
-    print('Temp={0:0.1f}°C  Humidity={1:0.1f}%'.format(temperature, humidity))
+    print('Temp={0:0.1f}°C  Humidity={1:0.1f}%'.format(temperature, humidity)) # Affichage de valeur de température et d'humidité   
 else:
     print('Failed to get reading. Try again!')
 
