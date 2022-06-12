d1dcf2f71fd62850084a9dd7102991dff29d293a
FERNANDES Mathieu <106628834+KCWYF@users.noreply.github.com>
2022-06-12 22:18:05 +0200

    Update moteur.cpp

--- RASPBERRY 2/MOTEUR/moteur.cpp
+++ RASPBERRY 2/MOTEUR/moteur.cpp
@@ -20,23 +20,23 @@ int main(){
 	reception_433 moteur;
         
 	while(1){
-	if( moteur.command() == "close") // Si moteur.command renvoie 9900 (voir programme de la récéption pour comprendre ce chiffre), on ferme la fenêtre
-	{
-        cout << "fermeture"; 
-	instanceI2C.close(); // On envoie alors la commande close de notre objet de la classe Moteur, permettant la fermeture
-	}
+		if( moteur.command() == "close") // Si moteur.command renvoie 9900 (voir programme de la récéption pour comprendre ce chiffre), on ferme la fenêtre
+		{
+		cout << "fermeture"; 
+		instanceI2C.close(); // On envoie alors la commande close de notre objet de la classe Moteur, permettant la fermeture
+		}
 
-	if(moteur.command() == "open") // Si moteur.command renvoie 9911 (voir programme de la récéption pour comprendre ce chiffre), on ouvre la fenêtre
-	{
-        cout << "ouverture";
-        instanceI2C.open(); // On envoie alors la commande open de notre objet de la classe Moteur, permettant l'ouverture
-	}
+		if(moteur.command() == "open") // Si moteur.command renvoie 9911 (voir programme de la récéption pour comprendre ce chiffre), on ouvre la fenêtre
+		{
+		cout << "ouverture";
+		instanceI2C.open(); // On envoie alors la commande open de notre objet de la classe Moteur, permettant l'ouverture
+		}
 
-	if(moteur.command() == "wait") // Si moteur.command renvoie 9966 (voir programme de la récéption pour comprendre ce chiffre), on ne fait rien dans ce cas la
-	{
-		cout << "on fait rien " << endl;
-		sleep(5);
-	}
+		if(moteur.command() == "wait") // Si moteur.command renvoie 9966 (voir programme de la récéption pour comprendre ce chiffre), on ne fait rien dans ce cas la
+		{
+			cout << "on fait rien " << endl;
+			sleep(5);
+		}
 	}
 }
 
